dbfc436a2c692c293a6e2222311664c740b6f2e4
Marina Kotelnikova <m.orlincky@gmail.com>
2024-10-12 21:43:53 +0300

    homework_10_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,5 @@
-def filter_by_state(lists_state: list, state: str="EXECUTED") -> list[str]:
-
-    """ Функция возвращает список словарей по ключу state с указанным значением"""
-
+def filter_by_state(lists_state: list, state: str = "EXECUTED") -> list[str]:
+    """Функция возвращает список словарей по ключу state с указанным значением"""
 
     new_lists = []
     for k in lists_state:
@@ -10,9 +8,7 @@ def filter_by_state(lists_state: list, state: str="EXECUTED") -> list[str]:
     return new_lists
 
 
-
-def sort_by_date(det_data: list, data: bool=True) -> list:
-
+def sort_by_date(det_data: list, data: bool = True) -> list:
     """Функция возвращает дату по убыванию"""
 
     sort_list = sorted(det_data, key=lambda x: x.get("date"), reverse=data)
